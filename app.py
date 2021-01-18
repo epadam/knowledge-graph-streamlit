9fac330fcc5a32216b21c281905f67e1286dff11
epadam <andrewe.epyy@gmail.com>
2021-01-18 05:22:14 +0100

    add

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+st.title(Knowledge Graph Overview)
+
+
+st.header('Structure of Knowledge Graph')
+
+st.subheader('Triple Store')
+
+
+
+st.header('Knowlege Graph Database')
+
+st.selectbox('', ('DBpedia', 'Freebase', 'Wikidata'))
+
+
+
+st.header('Building Knowledge Graph')
+
+
+
+st.subheader('Automatic Knowledge Graph Construction')
+
+st.write('Knowledge graph can be used to train language models like Bert, then use the trained model to buld the build the knowledge graph from structured or unstructured data')
+
+
+
+
+st.header('Knowledge Graph Applications')
+
+st.header('Knowledge Graph Compensation')
+
+st.subheader('Q&A Chatbot')
+
+
+st.subheader('Web and Social Media Monitoring')
+
+
+st.subheader('Recommendation')
+
+
+
+
+st.header('Knowledge Graph Resources')
